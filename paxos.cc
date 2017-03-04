f9326f4217b2cb2c049852f9e9b67589615c6061
Isaac Bowen <irbowen@umich.edu>
2017-03-04 17:46:56 -0500

    Notes

--- paxos.cc
+++ paxos.cc
@@ -27,6 +27,48 @@ void paxos::handle_msg(std::string msg) {
   // Step 2. Determine what kind of msg it is
   // Step 3. Call the correct function to hanlde it
   cout << "Got: " << msg << endl;
+  
+  // If client_req msg
+  // skip
+
+
+  // Incoming: prepare
+  // Arguments: n
+  // Acting as: Acceptor
+  // Check if you've seen an n higher than this one
+  // Outgoing: a prepare_accept or prepare_deny
+
+  // Incoming: prepare_accept
+  // Arguments: n_a, v_a (could be null)
+  // Acting as: Proposer
+  // Increment the count of # of prepare accepts you have gotten
+  // Outgoing: If this is more than a majority, send propose_value(n, v)
+
+  // Incoming: prepare_deny
+  // Arguments: n_p
+  // Acting as: Proposer
+  // If we got a higher n value, update our internal storage with this new value
+  // Outgoing:
+
+  // Incoming: Propose
+  // Arguments: n, v
+  // Acting as: Acceptor
+  // Outgoing: propose_accept(n), or propose_reject(n)
+
+  // Incoming: Propose_accept
+  // Arguments: n
+  // Acting as: Proposer
+  // Outgoing: decided(v)
+
+  // Incoming: propose_accept
+  // Arguments: n
+  // Acting as: Proposer
+  // Outgoing: decided(v)
+
+  // Incoming: decided
+  // Arguments: v
+  // Acting as: Learner
+  // Outgoing:
 }
 
 void paxos::setup_server(int port, std::string host) {
@@ -43,13 +85,10 @@ void paxos::setup_server(int port, std::string host) {
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
   addr.sin_port = htons(port);
 
-  if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof(opt_val)) < 0)
-    assert(false);
-  if (bind(serverfd, (struct sockaddr*) &addr, sizeof(addr)) < 0)
-    assert(false);
+  assert(setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof(opt_val)) == 0);
+  assert(bind(serverfd, (struct sockaddr*) &addr, sizeof(addr)) == 0);
   socklen_t addr_len = sizeof(addr);
-  if (getsockname(serverfd, (struct sockaddr *) &addr, &addr_len) < 0)
-    assert(false);
+  assert(getsockname(serverfd, (struct sockaddr *) &addr, &addr_len) == 0);
 
   struct sockaddr_storage their_addr;
   int numbytes;
