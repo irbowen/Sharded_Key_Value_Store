b4ba111250fddcaef89c0599338027663cabaef7
Pranav Ramarao <pranav.rr93@gmail.com>
2017-03-05 23:28:46 -0500

    added pseudo code for client lib

--- client_lib.cc
+++ client_lib.cc
@@ -3,8 +3,22 @@
 #include <vector>
 #include "client_lib.h"
 #include "replica.h"
+#include "message.h"
 
-void client_lib::add_chat_message(std::string msg){
-	
+void client_lib::add_chat_message(std::string chat_message){
+    Message msg;
+    msg.msg_type = MessageType::START_PREPARE;
+
+    int timeout = 1;
+    bool got_response = false;
+
+    // first, try to send to the primary and see if it works
+
+    while(!got_response){
+        // send the message as a broadcast with timeout = timeout, if no response, try again
+        timeout *= 2;
+    }
+    // the client's chat_message has been added
+    return;
 }
 
