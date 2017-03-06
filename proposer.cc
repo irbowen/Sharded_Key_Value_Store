6111cb315536f40369f2e99043258be5cae9ffb5
Pranav Ramarao <pranav.rr93@gmail.com>
2017-03-05 20:51:04 -0500

    added place holders for proposer

--- proposer.cc
+++ proposer.cc
@@ -7,3 +7,10 @@
 //
 
 #include "proposer.h"
+void Proposer::prepare_accept(int n_a, std::string value){}
+
+void Proposer::prepare_reject(int n_p){}
+
+void Proposer::propose_accept(int n){}
+
+void Proposer::propose_reject(int n_p){}
