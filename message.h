e193e0d77045193b9f43d51f991063bf35fac6f8
Pranav Ramarao <pranav.rr93@gmail.com>
2017-03-05 20:49:15 -0500

    added enum types for the messages

--- message.h
+++ message.h
@@ -11,11 +11,20 @@
 
 enum MessageType {
     PREPARE = 0,
-    PREPARE_ACCEPT 1
+    PREPARE_ACCEPT = 1,
+    PREPARE_REJECT = 2,
+    PROPOSE = 3,
+    PROPOSE_ACCEPT = 4,
+    PROPOSE_REJECT = 5,
+    BRDCST_LEARNERS = 6
 };
 class Message{
     
-    std::string message;
+public:
+    std::string value;
     MessageType msg_type;
+    int n_a;
+    int n_p;
+    int n;
 };
 #endif /* message_h */
