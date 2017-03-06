f9d7e701e2637fc4a9ac8589fbc47dfee9230180
Pranav Ramarao <pranav.rr93@gmail.com>
2017-03-05 23:30:16 -0500

    finishing up proposer code

--- proposer.cc
+++ proposer.cc
@@ -7,10 +7,50 @@
 //
 
 #include "proposer.h"
-void Proposer::prepare_accept(int n_a, std::string value){}
+Message Proposer::prepare_accept(int n_a, std::string value){
+    Message msg;
+    
+    count[proposal_number] += 1;
+    int quorum = (1 + tot_replicas) >> 2;
+    if(count[proposal_number] >= quorum){
+        msg.msg_type = MessageType::PROPOSE;
+        
+        if(n_a == -1){
+            // Propose the original value
+            msg.value = to_propose;
+        } else{
+            // Propose the already accepted value
+            msg.value = value;
+        }
+        return msg;
+    }
+    
+    // if quorum is not reached, the message type default is NO_ACTION
+    return msg;
+}
+Message Proposer::start_prepare(int proposal_number){
+    Message msg;
+    msg.msg_type = MessageType::PREPARE;
+    msg.prop_number = proposal_number;
+    return msg;
 
-void Proposer::prepare_reject(int n_p){}
+}
+Message Proposer::prepare_reject(int n_p){
+    // go back to prepare phase
+    proposal_number = n_p + 1;
+    Message msg;
+    msg.msg_type = MessageType::PREPARE;
+    msg.prop_number = proposal_number;
+    return msg;
+}
 
-void Proposer::propose_accept(int n){}
+Message Proposer::propose_accept(int n){
+    // nothing to do for now in this scenario
+    Message msg;
+    return msg;
+}
 
-void Proposer::propose_reject(int n_p){}
+Message Proposer::propose_reject(int n_p){
+    // if the proposal gets rejected, we are back to the prepare phase
+    return prepare_reject(n_p);
+}
--- proposer.h
+++ proposer.h
@@ -11,15 +11,28 @@
 
 #include <stdio.h>
 #include <string>
+#include <map>
+#include "message.h"
 
 class Proposer{
+private:
+    std::map<int, int> count;
+    
+    int tot_replicas;
+    int proposal_number;
+    
+    /* The value to be proposed */
+    std::string to_propose;
+
 public:
-    void prepare_accept(int n_a, std::string value);
+    Message start_prepare(int proposal_number);
     
-    void prepare_reject(int n_p);
+    Message prepare_accept(int n_a, std::string value);
     
-    void propose_accept(int n);
+    Message prepare_reject(int n_p);
     
-    void propose_reject(int n_p);
+    Message propose_accept(int n);
+    
+    Message propose_reject(int n_p);
 };
 #endif /* proposer_h */
