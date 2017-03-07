fdb46aa70ce9fc738ff9a5b1993bca61293a4b4d
Pranav Ramarao <pranav.rr93@gmail.com>
2017-03-07 12:13:20 -0500

    adding node.h

---
+++ node.h
@@ -0,0 +1,19 @@
+//
+//  server.h
+//  ChatService
+//
+//  Created by Pranav Ramarao on 3/6/17.
+//  Copyright © 2017 Pranav Ramarao. All rights reserved.
+//
+
+#ifndef server_h
+#define server_h
+
+
+#endif /* server_h */
+
+struct node{
+    int port;
+    std::string host;
+    // any other info regarding a server
+};
