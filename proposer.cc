df503766e04652aaf7fab753a8c6070ae48603a1
Pranav Ramarao <pranav.rr93@gmail.com>
2017-03-08 02:05:57 -0500

    added total replicas field. Fixed bug with >> 2 instead of >> 1. Added proposal number to the reply message

--- proposer.cc
+++ proposer.cc
@@ -11,15 +11,17 @@
 Message* Proposer::prepare_accept(int n_a, std::string value){
     Message *msg = new Message;
     count[proposal_number] += 1;
-    int quorum = (1 + tot_replicas) >> 2;
+    int quorum = (1 + tot_replicas) >> 1;
     if(count[proposal_number] >= quorum){
         msg->msg_type = MessageType::PROPOSE;
         if(n_a == -1){
             // Propose the original value
             msg->value = to_propose;
+            msg->prop_number = proposal_number;
         } else{
             // Propose the already accepted value
             msg->value = value;
+            msg->prop_number = proposal_number;
         }
     }
     // if quorum is not reached, the message type default is NO_ACTION
@@ -46,4 +48,7 @@ Message* Proposer::propose_accept(int n){
 Message* Proposer::propose_reject(int n_p){
     // if the proposal gets rejected, we are back to the prepare phase
     return start_prepare(n_p + 1);
+}
+void Proposer::set_tot_replicas(int tot){
+    tot_replicas = tot;
 }
--- proposer.h
+++ proposer.h
@@ -20,11 +20,13 @@ private:
     
     int tot_replicas;
     int proposal_number;
-    
+
+public:
     /* The value to be proposed */
     std::string to_propose;
 
-public:
+    void set_tot_replicas(int tot_replicas);
+
     Message* start_prepare(int proposal_number);
     
     Message* prepare_accept(int n_a, std::string value);
