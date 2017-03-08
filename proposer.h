8cecc9212bff2de48f1f890de54f56564d5ab7d7
Isaac Bowen <irbowen@umich.edu>
2017-03-08 12:30:58 -0500

    Fixing compile sign warnings -> ints to size_t

--- proposer.h
+++ proposer.h
@@ -9,26 +9,26 @@
 #include "node.h"
 
 class Proposer{
-private:
-    std::map<int, int> count;
-    
+  private:
+    std::map<int, size_t> count;
+
     size_t quorum;
     int proposal_number;
     std::vector<node> replicas;
-    
-public:
+
+  public:
     /* The value to be proposed */
     std::string to_propose;
-    
+
     void init(vector<node> _replicas);
     Message* start_prepare(int proposal_number);
-    
+
     Message* prepare_accept(int n_a, std::string value);
-    
+
     Message* prepare_reject(int n_p);
-    
+
     Message* propose_accept(int n);
-    
+
     Message* propose_reject(int n_p);
 };
 #endif /* proposer_h */
--- replica.h
+++ replica.h
@@ -17,36 +17,36 @@
 #include "proposer.h"
 
 class replica {
-private:
+  private:
     // server info
     int port;
     std::string host;
-    
+
     // replica id
-    int id;
-    
+    size_t id;
+
     // current view number that this replica thinks it is in
-    int cur_view_num;
-    
+    size_t cur_view_num;
+
     // total number of replicas in the system
     size_t num_replicas = 0;
-    
+
     network net;
-    
+
     // list of replicas in the system
     std::vector<node> replicas;
-    
+
     void handle_msg(Message*);
-    
+
     /* Adds all the replicas in the system to the receiver list of the reply message */
     void add_all_to_receiver_list(Message *reply);
-    
+
     /* Replica can take up any one of the following roles */
     Acceptor acceptor;
     Proposer proposer;
     Learner learner;
-    
-public:
+
+  public:
     replica(int _port, std::string _host, int _id, std::string _config_file);
     void start();
 };
