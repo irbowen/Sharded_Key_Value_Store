30356e30a2c07b3f47463223f618a96f48529161
Pranav Ramarao <pranav.rr93@gmail.com>
2017-03-07 21:12:46 -0500

    removed placeholder that was used for testing

--- proposer.cc
+++ proposer.cc
@@ -21,17 +21,6 @@ Message* Proposer::prepare_accept(int n_a, std::string value){
             // Propose the already accepted value
             msg->value = value;
         }
-        // TODO: add this member to the class, keeping it to compile
-        std::vector<node> acceptor_list;
-        
-        // TEST
-        acceptor_list.push_back(node(2000, LOCALHOST));
-        acceptor_list.push_back(node(2001, LOCALHOST));
-        acceptor_list.push_back(node(2002, LOCALHOST));
-
-        for(auto item: acceptor_list){
-            msg->receivers.push_back(item);
-        }
     }
     // if quorum is not reached, the message type default is NO_ACTION
     return msg;
@@ -40,17 +29,6 @@ Message* Proposer::start_prepare(int proposal_number){
     Message *msg = new Message;
     msg->msg_type = MessageType::PREPARE;
     msg->prop_number = proposal_number;
-    
-    // TODO: add this member to the class, keeping it to compile
-    std::vector<node> acceptor_list;
-    // TEST
-    acceptor_list.push_back(node(2000, LOCALHOST));
-    acceptor_list.push_back(node(2001, LOCALHOST));
-    acceptor_list.push_back(node(2002, LOCALHOST));
-    
-    for(auto item: acceptor_list){
-        msg->receivers.push_back(item);
-    }
     return msg;
 }
 
