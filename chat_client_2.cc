d4ded9fbc211143eec3a966bfcd7e76556991261
Isaac Bowen <irbowen@umich.edu>
2017-03-09 18:56:32 -0500

    Making better chat clients

--- chat_client_2.cc
+++ chat_client_2.cc
@@ -6,8 +6,7 @@ using namespace std;
 int main(){
   client_lib c_lib(6001, "127.0.0.1");
 
-  //vector<string> messages = {"Hello", "How are you", "Do you like apples?"};
-  vector<string> messages = {"EECS 498 test"};
+  vector<string> messages = {"C2 msg 1", "C2 msg 2", "C2 msg 3"};
   for (auto &message : messages){
     cout << "Trying to add " << message << endl;
     c_lib.add_chat_message(message);
--- chat_client_3.cc
+++ chat_client_3.cc
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main(){
-  client_lib c_lib(6000, "127.0.0.1");
+  client_lib c_lib(6002, "127.0.0.1");
 
-  vector<string> messages = {"Client one chat one", "Client one chat two"};
+  vector<string> messages = {"C3 msg 1", "C3 msg 2", "C3 msg 3"};
   for (auto &message : messages){
     cout << "Trying to add " << message << endl;
     c_lib.add_chat_message(message);
