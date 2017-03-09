976c5f5e9609ae5e230c9d58021b1a912e9642af
Isaac Bowen <irbowen@umich.edu>
2017-03-09 18:57:08 -0500

    Startup scripts

---
+++ start_clients.sh
@@ -0,0 +1,4 @@
+./chat_client_1.out
+./chat_client_2.out
+./chat_client_3.out
+./chat_client_4.out
--- start_paxos.sh
+++ start_paxos.sh
@@ -1,3 +1,3 @@
-./paxos_server --port 9000 --host 127.0.0.1 --config config.txt --id 0 &
-./paxos_server --port 9001 --host 127.0.0.1 --config config.txt --id 1 &
-./paxos_server --port 9002 --host 127.0.0.1 --config config.txt --id 2 &
+./paxos_server --port 8000 --host 127.0.0.1 --config config.txt --id 0 &
+./paxos_server --port 8001 --host 127.0.0.1 --config config.txt --id 1 &
+./paxos_server --port 8002 --host 127.0.0.1 --config config.txt --id 2 &
