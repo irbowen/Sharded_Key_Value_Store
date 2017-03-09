e192208c7b6bdefdd793c7743df3e61dae5e1a85
Pranav Ramarao <pranav.rr93@gmail.com>
2017-03-09 18:04:50 -0500

    added some important fields

--- learner.cc
+++ learner.cc
@@ -9,20 +9,24 @@ Message* Learner::update_vote(int in_view, int seq_num, string value){
     lock_guard<mutex> lock(m);
     Message *reply = new Message;
     
+    Message m;
+    m.value = value;
+    
     score_map[in_view].tally += 1;
-    score_map[in_view].value = value;
+    score_map[in_view].value = m.get_value();
     // If this is the message that made us equal to the qurom
     if (score_map[in_view].tally == quorum) {
         // We should broadcast a proposal learned msg to everyone
         reply->msg_type = MessageType::PROPOSAL_LEARNT;
         reply->seq_num = seq_num;
-
+        reply->value = value;
+        reply->view_num = in_view;
         // commit message to local chat log
         // set seq_num -> seq_num + 1
         if (static_cast<int>(chat_log.size()) <= seq_num) {
             chat_log.resize(seq_num + 1);
         }
-        chat_log.at(seq_num) = value;
+        chat_log.at(seq_num) = m.get_value();
         print_log();
     }
     return reply;
