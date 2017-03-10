3b78bd0526d72f76d0f120ea4cd91167bb798f3d
Isaac Bowen <irbowen@umich.edu>
2017-03-09 21:42:14 -0500

    I am the testcases

--- chat_client_5.cc
+++ chat_client_5.cc
@@ -4,13 +4,9 @@
 using namespace std;
 
 int main(){
-  client_lib c_lib(6004, "127.0.0.1", "config.txt");
+  client_lib c_lib(6005, "127.0.0.1", "config5.txt");
 
-  vector<string> messages;
-  for (int i = 0; i < 100; i++) {
-    messages.push_back("C4M" + to_string(i));
-  }
-
+  vector<string> messages = {"C55M1", "C55M2", "C55M3"};
   for (auto &message : messages){
     cout << "Trying to add " << message << endl;
     c_lib.add_chat_message(message);
