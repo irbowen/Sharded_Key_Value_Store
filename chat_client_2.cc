75d0a4cd741613274008bd6201a241393d5ce34e
Isaac Bowen <irbowen@umich.edu>
2017-03-09 20:15:15 -0500

    This is a bug

--- chat_client_2.cc
+++ chat_client_2.cc
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
   client_lib c_lib(6001, "127.0.0.1");
 
-  vector<string> messages = {"C2 msg 1", "C2 msg 2", "C2 msg 3"};
+  vector<string> messages = {"C2M1", "C2M2", "C2M3", "C2M4"};
   for (auto &message : messages){
     cout << "Trying to add " << message << endl;
     c_lib.add_chat_message(message);
