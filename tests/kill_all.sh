27965405e1b537b2b8b7f0a23636b704df6377c4
Isaac Bowen <irbowen@umich.edu>
2017-04-15 21:04:21 -0400

    Removing

--- tests/kill_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -e "We are killing all the paxos servers and the chat clients\n"
-
-ps aux | grep paxos_server | awk '{print $2}' | xargs kill
-ps aux | grep chat_client_ | awk '{print $2}' | xargs kill
-ps aux | grep kv_client_ | awk '{print $2}' | xargs kill
